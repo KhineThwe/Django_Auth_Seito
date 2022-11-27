bbc2f5a87869c9f543ff532f9e04557b9577090b
Khine <khinezar70122107@gmail.com>
2022-11-26 18:58:36 -1200

    sign up

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    count = User.objects.count()
+    return render(request,'home.html',{
+        'count':count
+    })
 
 # def signup(request):
 #     return render(request,'registration/signup.html')
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method  == 'POST':
+         form = UserCreationForm(request.POST)
+         if form.is_valid():#password မှန်လား username မှန်လားတွေပါစစ်ပေးတာ
+            form.save()
+            return redirect('home')
+    else:#if method is get method
+        form = UserCreationForm()
     return render(request,'registration/signup.html',{
         'form':form
     })
+   
