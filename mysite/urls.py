fd8c219e845a85a9b79d8a67693d83c7062d7311
Khine <khinezar70122107@gmail.com>
2023-01-16 10:06:15 -1200

    classed based view

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('',views.home,name="home"),
     path('signup/',views.signup,name="signup"),
     path('accounts/',include('django.contrib.auth.urls')),#for login
-    
+    path('secret/',views.secret_page,name='secret'),
+    path('secret2/',views.SecretPage.as_view(),name='secret2'),
     path('admin/', admin.site.urls),
 ]
